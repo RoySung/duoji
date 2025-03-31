22733ae5593cb65ddeda9a514cdb009a4d5a8099
Roy Sung <roysunghan@gmail.com>
2025-03-31 11:39:24 +0800

    :sparkles: add GitHub Pages deployment workflow

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,13 +1,17 @@
 //@ts-check
 
-
 const { composePlugins, withNx } = require('@nx/next');
+
+// Get repository name from package.json or environment variable
+const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'duoji';
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
   output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? `/${repo}` : '',
+  assetPrefix: process.env.NODE_ENV === 'production' ? `/${repo}/` : '',
   nx: {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
