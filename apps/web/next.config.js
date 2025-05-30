afc45a63fbf7e309b9941caa02bb0e93ea820ed5
RoySung <roysung@RoySungs-MacBook-Pro-2.local>
2025-05-30 16:26:09 +0800

    🐛 fix: fix issue about deployment for cloudflare page

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,15 +3,15 @@
 const { composePlugins, withNx } = require('@nx/next');
 
 // Get repository name from package.json or environment variable
-const repo = process.env.GITHUB_REPOSITORY?.split('/')[1] || 'duoji';
+const repo = process.env.GITHUB_REPOSITORY?.split('/')[1];
 
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? `/${repo}` : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? `/${repo}/` : '',
+  basePath: process.env.NODE_ENV === 'production' && repo ? `/${repo}` : '',
+  assetPrefix: process.env.NODE_ENV === 'production' && repo ? `/${repo}/` : '',
   nx: {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
